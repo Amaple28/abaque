88071e32f4f4dd822bc7bb96f3e47a72f4fa9ca6
Maisa Gabriela <maisagabirodrigues@gmail.com>
2022-10-28 15:38:37 -0300

    Finalizando front abaque

--- js/curiosidades.js
+++ js/curiosidades.js
@@ -5,7 +5,7 @@ function curiosidades () {
 
     var curiosidade = new Array();
 
-    curiosidade[0] = "1º) Você sabia que o nome ENIAC significa Eletronical Numerical Integrator and Computer, traduzindo, Integrador e Computador Numérico Eletrônico. O ENIAC funcionava através de um sistema composto por circuitos e válvulas, que ocupavam muito espaço, mas cumprindo seu objetivo de realizar cálculos complexos. Mesmo assim, nem se comparava à minha velocidade de processamento. Olha como ele era:"; 
+    curiosidade[0] = "Você sabia que o nome ENIAC significa Eletronical Numerical Integrator and Computer, traduzindo, Integrador e Computador Numérico Eletrônico. O ENIAC funcionava através de um sistema composto por circuitos e válvulas, que ocupavam muito espaço, mas cumprindo seu objetivo de realizar cálculos complexos. Mesmo assim, nem se comparava à minha velocidade de processamento. Olha como ele era:"; 
 
     var aleatorio = Math.floor(Math.random() * curiosidade.length);
 
