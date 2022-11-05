56df912b54490b77c70fc4e21822adda547544fd
Lucas Eleutério Bueno <leleuteiobueno@gmail.com>
2022-11-05 09:55:03 -0300

    Médio finalizado

--- js/medio.js
+++ js/medio.js
@@ -4,17 +4,117 @@ var cronometro;
 
 var answers = [];
 
-const questions = []
-const correctAnswers = [
+const questions = [
     {
-        name: 'options1',
-        answer: '4'
+        name: null,
+        question: "67-76=?",
+        options:[
+            {
+                answer: "-9",
+                correct: true
+            },
+            {
+                answer: "-7",
+                correct: false
+            },
+            {
+                answer: "10",
+                correct: false
+            },
+        ]
     },
     {
-        name: 'options2',
-        answer: '3'
+        name: null,
+        question: "10÷20=?",
+        options:[
+            {
+                answer: "0.5",
+                correct: true
+            },
+            {
+                answer: "4",
+                correct: false
+            },
+            {
+                answer: "30",
+                correct: false
+            },
+        ]
+    },
+    {
+        name: null,
+        question: "7x5=?",
+        options:[
+            {
+                answer: "37",
+                correct: false
+            },
+            {
+                answer: "14",
+                correct: false
+            },
+            {
+                answer: "35",
+                correct: true
+            },
+        ]
+    },
+    {
+        name: null,
+        question: "77+33=?",
+        options:[
+            {
+                answer: "90",
+                correct: false
+            },
+            {
+                answer: "100",
+                correct: false
+            },
+            {
+                answer: "110",
+                correct: true
+            },
+        ]
+    },
+    {
+        name: null,
+        question: "9÷2=?",
+        options:[
+            {
+                answer: "4",
+                correct: false
+            },
+            {
+                answer: "4.5",
+                correct: true
+            },
+            {
+                answer: "0",
+                correct: false
+            },
+        ]
+    },
+    {
+        name: null,
+        question: "10x10x10=?",
+        options:[
+            {
+                answer: "100",
+                correct: false
+            },
+            {
+                answer: "1000",
+                correct: true
+            },
+            {
+                answer: "111",
+                correct: false
+            },
+        ]
     },
 ]
+
 
 function iniciarCronometro() {
     cronometro = setInterval('atualizarCronometro()', 1000);
@@ -39,7 +139,28 @@ function exibirTelaJogo() {
 
 function comecar() {
     var tempo = new Date();
-    tempoInicial = tempo.getTime();     
+    tempoInicial = tempo.getTime();
+    
+    for (let index = 0; index <= 1 ; index++) {
+        const elementIndex = Math.floor(Math.random() * questions.length);
+        if(questions[elementIndex].name == null){
+            questions[elementIndex].name = "options" + (index+1);
+
+            document.getElementById("question" + (index+1)).innerHTML = questions[elementIndex].question;
+            
+            document.getElementById(`label${(index+1)}-1`).innerHTML = questions[elementIndex].options[0].answer;
+            document.getElementById(`label${(index+1)}-2`).innerHTML = questions[elementIndex].options[1].answer;
+            document.getElementById(`label${(index+1)}-3`).innerHTML = questions[elementIndex].options[2].answer;
+
+            document.getElementById(`option${(index+1)}-1`).value = questions[elementIndex].options[0].answer;
+            document.getElementById(`option${(index+1)}-2`).value = questions[elementIndex].options[1].answer;
+            document.getElementById(`option${(index+1)}-3`).value = questions[elementIndex].options[2].answer;
+
+        }else{
+            index--;
+        }
+        
+    }
 }
 
 function checkFinished(asnwer, name){
@@ -64,7 +185,7 @@ function checkAnswers(){
     let wrong = 0;
 
     answers.forEach(answer => {
-        if(correctAnswers.find(x => x.name == answer.name && x.answer === answer.answer) != undefined){
+        if(questions.find(x => x.name === answer.name).options.find(x => x.answer == answer.answer).correct){
             correct++;
         }else{
             wrong++;
